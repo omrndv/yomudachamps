398614a759e3a70515d02ee73c9fc896af7506d3
omrndv <umarnadiv@gmail.com>
2026-06-29 21:40:13 +0700

    Include Bootstrap JS bundle in bracket page to fix modal trigger not working

--- resources/views/pages/bracket.blade.php
+++ resources/views/pages/bracket.blade.php
@@ -975,6 +975,9 @@
         </div>
     </div>
 
+    <!-- Bootstrap Bundle with Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
     <script>
     document.addEventListener('DOMContentLoaded', function() {
         const headerBar = document.getElementById('roundHeadersBar');
