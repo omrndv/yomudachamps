34d62a9e6491b773c981b4c579c100cb2e7fa09c
omrndv <umarnadiv@gmail.com>
2026-07-03 16:17:00 +0700

    fix: add no-cache headers to checkStatusAjax response to prevent LiteSpeed cache on Hostinger

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -468,7 +468,9 @@ class HomeController extends Controller
     
         return response()->json([
             'status' => $team->status
-        ]);
+        ])->header('Cache-Control', 'no-cache, no-store, must-revalidate')
+          ->header('Pragma', 'no-cache')
+          ->header('Expires', '0');
     }
 
     public function redirectCertificate()
