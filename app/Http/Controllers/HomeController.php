ea0f4e2793981d1bbd24e3572a99f9665bf7a8aa
omrndv <umarnadiv@gmail.com>
2026-07-03 07:51:27 +0700

    feat: re-enable GoPay QRIS channel for checkout with stable static QRIS

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -177,8 +177,7 @@ class HomeController extends Controller
             ];
         }
 
-        // Load GoPay QRIS (Self-hosted) if enabled (Sembunyikan sementara untuk besok pagi)
-        /*
+        // Load GoPay QRIS (Self-hosted) if enabled
         if (\App\Models\Setting::getVal('payment_gateway_gopay_qris', 'on') === 'on') {
             $channels[] = (object)[
                 'code' => 'GOPAY_QRIS',
@@ -187,7 +186,6 @@ class HomeController extends Controller
                 'active' => true,
             ];
         }
-        */
 
         return view('payment', compact('team', 'channels'));
     }
