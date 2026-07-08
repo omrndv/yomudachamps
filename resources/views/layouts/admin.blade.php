e2cfe84afa6da062d6e7fb152087771b6c4d52d2
omrndv <umarnadiv@gmail.com>
2026-07-08 18:26:33 +0700

    fix: constrain desktop sidebar bounds using overflow hidden to force inner navigation scroll

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -48,6 +48,7 @@
             border-right: 1px solid rgba(255, 255, 255, 0.06);
             display: flex;
             flex-direction: column;
+            overflow: hidden;
         }
 
         .sidebar-header {
